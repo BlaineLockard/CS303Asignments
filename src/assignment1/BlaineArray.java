15a355ffea5df767fe3537f4a0224858d250c752
Blaine Lockard <blainelockard@gmail.com>
2026-02-04 15:11:44 -0600

    add and remove functions, and added input file

--- src/assignment1/BlaineArray.java
+++ src/assignment1/BlaineArray.java
@@ -1,15 +1,18 @@
 package assignment1;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class BlaineArray<T> {
     private T[] array;
+    private int size = 0;
 
     public BlaineArray(){
     }
 
     public BlaineArray(T[] array){
         this.array = array;
+        size = array.length;
     }
 
     public T get(int index) throws ArrayIndexOutOfBoundsException{
@@ -25,18 +28,53 @@ public class BlaineArray<T> {
         return -1;
     }
 
-    public T[] set(int index, T newEntry) throws ArrayIndexOutOfBoundsException{
+    public T[] set(int index, T newItem) throws ArrayIndexOutOfBoundsException{
         // Put the objects into an object array since T[] cannot be initialized
         Object[] returnArray = new Object[2];
         returnArray[0] = array[index];
-        returnArray[1] = newEntry;
+        returnArray[1] = newItem;
 
-        array[index] = newEntry;
+        array[index] = newItem;
 
         // cast the object array to a T array
         return (T[]) returnArray;
     }
 
+    public void add(T newItem){
+        if(size == 0){ // if the array hasn't been made yet
+            size++;
+            Object[] newArray = new Object[]{newItem};
+            array = (T[]) newArray;
+        }
+        else{ // if the array has items
+            size++;
+            array =  Arrays.copyOf(array, size);
+            array[array.length - 1] = newItem;
+        }
+    }
+
+    public T remove(int index) throws ArrayIndexOutOfBoundsException{
+        // Get the item that will be removed and the last item in the array
+        T item = array[index];
+        T lastItem = array[array.length - 1];
+        size--;
+
+        // copy the array minus the last item
+        array = Arrays.copyOf(array, size);
+
+        // move items after the removing index back one space
+        for(int i = index; i < array.length-1; i++){
+            array[i] = array[i + 1];
+        }
+
+        // put the last item in the last position
+        array[array.length - 1] = lastItem;
+        return item;
+    }
+
+    public int size(){
+        return size;
+    }
 
     public String toString(){
         String returnString = "";
--- src/assignment1/Main.java
+++ src/assignment1/Main.java
@@ -1,14 +1,29 @@
 package assignment1;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
         BlaineArray<Integer> nums = new BlaineArray<>(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
         System.out.println("Item at index 3 is " + nums.get(3));
 
         Object[] replacement = nums.set(3, 100);
-        System.out.println("replacing " + replacement[1] + " with " + replacement[0]);
+        System.out.println("Replacing " + replacement[1] + " with " + replacement[0]);
 
         System.out.println("Item at index 3 is " + nums.get(3));
+        nums.add(13);
+
+        int itemPos = nums.hasItem(6);
+        if (itemPos != -1) {
+            System.out.println("Found 6 at index " + itemPos);
+        }
+        else{
+            System.out.println("Could not find a 6");
+        }
+
+        System.out.println("Removing item at index 4");
+        nums.remove(4);
+
         System.out.println(nums.toString());
     }
 }
